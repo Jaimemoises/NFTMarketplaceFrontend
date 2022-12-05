ef5c51376d98a8ae48e031d47feedfdb28bba022
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-05 00:46:40 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static(__dirname + '/dist/angular-ecommerce'));
 app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-ecommerce/index.html'));});
 
