3e1ae4379d663694dea17223e10c8e5f2ea76939
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-05 10:23:15 +0000

    initial commit

--- server.js
+++ server.js
@@ -12,3 +12,22 @@ app.use(express.static(__dirname + '/dist/angular-ecommerce'));
 app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-ecommerce/index.html'));});
 
 app.listen(process.env.PORT || 8080);
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://nftmarketplaceecommerce.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
