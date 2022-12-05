c8544255b1dfafadedb75ef39d1c4ea306849c28
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-05 10:36:57 +0000

    initial commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://nftmarketplaceecommerce.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
