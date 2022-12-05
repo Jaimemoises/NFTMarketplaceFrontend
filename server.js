5de11e5f1945ecad75749d9589cd937b545c526d
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-05 10:29:14 +0000

    initial commit

--- server.js
+++ server.js
@@ -2,21 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
-    next();
-});
-
-app.use(express.static(__dirname + '/dist/angular-ecommerce'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-ecommerce/index.html'));});
-
-app.listen(process.env.PORT || 8080);
+//app.use(function(req, res, next){
+    ///res.header("Access-Control-Allow-Origin", "*");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    //next();
+//});
 
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://nftmarketplaceecommerce.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://nftmarketplaceecommerce.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -31,3 +26,9 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+
+app.use(express.static(__dirname + '/dist/angular-ecommerce'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/angular-ecommerce/index.html'));});
+
+app.listen(process.env.PORT || 8080);
+
