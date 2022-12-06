7c9389f38935582102faca9b7ba3659665ddde6e
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-06 00:34:31 +0000

    initial commit

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc:{
-        clientId: '0oa7h27vy7Yij9W1W5d7',
-        issuer: 'https://dev-48468111.okta.com/oauth2/default',
+        clientId: '0oa7ivgypgtzOAcfC5d7',
+        issuer: 'https://dev-48468111.okta.com',
         //redirectUri: 'http://localhost:4200/login/callback',
         redirectUri: 'https://www.jaimedev7.com/login/callback',
         scopes: ['openid', 'profile', 'email']
