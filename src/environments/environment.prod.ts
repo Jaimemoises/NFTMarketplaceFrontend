64c0fff8c83e9ca67b8f04d9b07a0b1b8a24ae12
Jaime Moises <99954734+Jaimemoises@users.noreply.github.com>
2022-12-08 09:30:19 +0000

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+//export const environment = {
+  //production: true
+//};
+
 export const environment = {
-  production: true
+  production: false,
+  ecommerceApiUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51MCLPmKEZxuUlt0C9x1Tqx9wplVBlqzV6OM47Yycl5z20Ly2iEZ4QdgaC9Q0Xe5vDviSQzalyXTTkZZg42OxO3eX00cS1s2DfI"
 };
